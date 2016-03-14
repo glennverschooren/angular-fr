ecbe5399de440849b9d21f026a7ebf532c072939
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2016-03-14 09:50:36 +0100

    create initial example app

--- examles/src/app.ts
+++ examles/src/app.ts
@@ -1,22 +1,20 @@
 ///<reference path="../../index.d.ts"/>
 
-import 'angular2/bundles/angular2-polyfills';
-import {Component, View} from 'angular2/core';
-import {bootstrap} from 'angular2/platform/browser';
+import '../../node_modules/angular2/bundles/angular2-polyfills';
+import {Component, View} from '../../node_modules/angular2/core';
+import {bootstrap} from '../../node_modules/angular2/platform/browser';
 import { FrFormComponent } from '../../index';
-
-//console.log(FrFormComponent);
 
 @Component({
     selector: 'app',
+    directives: [FrFormComponent],
     template: `<div>
-                   FormRenderer
                    <fr-form></fr-form>
                </div>`
 })
 export class App {
     constructor() {
-        console.log('test my app');
+        console.log('Initialize app');
     }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
-///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
-/*
- * Providers provided by Angular
- */
-//import * as ngCore from 'angular2/core';
-//import * as browser from 'angular2/platform/browser';
-
-//import {FrForm} from './app/app';
-
-/**
- * Bootstrap our formRenderer with our top level component `FrForm` and 
- * inject our Services and Providers into Angular's dependency injections
- */
-// export function main() {
-//     return browser.bootstrap(FrForm).catch(err => console.log(err));
-// }
+/// <reference path="../typings/browser.d.ts"" />
 
 import { CONST_EXPR } from 'angular2/src/facade/lang';
 import { FrFormComponent } from './FrFormComponent';
